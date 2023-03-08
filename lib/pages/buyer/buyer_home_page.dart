a4b03dc6c8e325a3f3a07514efd854bcfb165763
RanopRamah <101007427+RanopRamah@users.noreply.github.com>
2023-03-08 22:31:15 +0700

    Ranop Commit 2

--- lib/pages/buyer/buyer_home_page.dart
+++ lib/pages/buyer/buyer_home_page.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BuyerHomePage extends StatefulWidget {
-  const BuyerHomePage({super.key,});
+  const BuyerHomePage({
+    super.key,
+  });
 
   @override
   State<BuyerHomePage> createState() => _BuyerHomePageState();
@@ -11,25 +14,46 @@ class _BuyerHomePageState extends State<BuyerHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child:Container(
+        body: SingleChildScrollView(
+      child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 30, top: 40),
+                width: double.infinity,
+                height: 169,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/back_home.png'))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Good Morning,',
+                      style: TextStyle(
+                        fontFamily: 'DM Sans',
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Color(0xffC5A2A2),
+                      ),
+                    ),
 
-        width: double.infinity,
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 162,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/back_home.png')
-                )
-              ),
-            )
-          ],
-        ),
-      ),
-      )
-    );
+                    Text(
+                      'Raffi Nauval',
+                      style: TextStyle(
+                        fontFamily: 'DM Sans',
+                        fontWeight: FontWeight.w700,
+                        fontSize: 32,
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+    ));
   }
 }
