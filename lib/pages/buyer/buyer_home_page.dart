468910873446c50772ce772a9eed817d60606313
RanopRamah <101007427+RanopRamah@users.noreply.github.com>
2023-03-10 21:51:21 +0700

    Ranop Commit 3

--- lib/pages/buyer/buyer_home_page.dart
+++ lib/pages/buyer/buyer_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mntmarketpay/pages/buyer/home-widget/inventory.dart';
+import 'package:mntmarketpay/pages/buyer/home-widget/transaction.dart';
 
 class BuyerHomePage extends StatefulWidget {
   const BuyerHomePage({
@@ -15,45 +17,121 @@ class _BuyerHomePageState extends State<BuyerHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-      child: Container(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 30, top: 40),
+            child: Container(
+              color: Color(0xfffbfbfb),
                 width: double.infinity,
-                height: 169,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/images/back_home.png'))),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Good Morning,',
-                      style: TextStyle(
-                        fontFamily: 'DM Sans',
-                        fontWeight: FontWeight.w500,
-                        fontSize: 16,
-                        color: Color(0xffC5A2A2),
-                      ),
-                    ),
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 30, right: 30),
+                        width: double.infinity,
+                        height: 182,
+                        decoration: BoxDecoration(
+                            color: Color(0xff458EDE),
+                            image: DecorationImage(
+                                image:
+                                    AssetImage('assets/images/back_home.png'))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const <Widget>[
+                              Text(
+                                'Good Morning,',
+                                style: TextStyle(
+                                  fontFamily: 'DM Sans',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16,
+                                  color: Color(0xffC5A2A2),
+                                ),
+                              ),
+                              Text(
+                                'Raffi Nauval',
+                                style: TextStyle(
+                                  fontFamily: 'DM Sans',
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 32,
+                                  color: Color(0xffffffff),
+                                ),
+                              ),
+                            ],
+                          ),),
+                          Container(
 
-                    Text(
-                      'Raffi Nauval',
-                      style: TextStyle(
-                        fontFamily: 'DM Sans',
-                        fontWeight: FontWeight.w700,
-                        fontSize: 32,
-                        color: Color(0xffffffff),
+                            width: 61,
+                            height: 61,
+                            decoration: BoxDecoration(
+                                color: Color(0xffFFCB0A),
+                                borderRadius: BorderRadius.circular(30)),
+                            child:Center(
+                              child: Text('RN',style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'DM Sans',
+                                color: Colors.white
+                            ),),)
+                          )
+                        ]
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )),
-    ));
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        width: double.infinity,
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Image.asset('assets/images/wallet.png',width: 16,height: 14,),
+                                SizedBox(width: 10,),
+                                Text('Balance',style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: 'DM Sans',
+                                    color: Color(0xffaaaaaa)
+                                ),),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Text('Rp',style: TextStyle(
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: 'DM Sans',
+                                    color: Color(0xff868383)
+                                ),),
+                                SizedBox(width: 10,),
+                                Text('12,000,000',style: TextStyle(
+                                    fontSize: 42,
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: 'DM Sans',
+                                    color: Color(0xff000000)
+                                ),),
+                              ],
+                            ),
+SizedBox(
+  height: 30,
+),
+
+                      TransactionHistory(),
+                            SizedBox(
+                              height: 30,
+                            ),
+                            InventoryList()
+                          ],
+                        ),
+                      )
+
+                    ]
+                )
+            )
+        )
+    );
   }
 }
